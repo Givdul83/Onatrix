927cecd95d89fd037bcb2450478e6f44c22f1fa8
Ludvig <ludvig.vinoy@hotmail.com>
2024-09-19 14:39:54 +0200

    sxcript

--- Onatrix/wwwroot/scripts/main.js
+++ Onatrix/wwwroot/scripts/main.js
@@ -1,6 +1,8 @@
 import { fadeInOnScroll } from './fadeScript.js';
+import { rotateOnScroll } from './rotateScript.js'
 
 
 document.addEventListener('DOMContentLoaded', function () {
     fadeInOnScroll();
+    rotateOnScroll();
 });
---
+++ Onatrix/wwwroot/scripts/rotateScript.js
@@ -0,0 +1,24 @@
+﻿export function rotateOnScroll() {
+    const rotateElements = document.querySelectorAll('.rotate');
+
+    function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top < (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.bottom > 0
+        );
+    }
+
+    function onScroll() {
+        rotateElements.forEach(el => {
+            if (isElementInViewport(el)) {
+                el.classList.add('rotated');
+            } else {
+                el.classList.remove('rotated');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', onScroll);
+    onScroll();
+}
